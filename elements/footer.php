747e788fb98f1ed3629ce1729c126e8a4ff78cfe
AleisterFox <ericaguirrez@hotmail.com>
2024-04-11 18:03:46 -0600

    vistas principales creadas y vista de producto

---
+++ elements/footer.php
@@ -0,0 +1,38 @@
+<footer>
+      <div class="top">
+        <a href="#hero">
+          <figure>
+            <img src="img/logo.jpg" alt="" />
+          </figure>
+        </a>
+        <ul>
+          <li><a href="">Historia</a></li>
+          <li>
+            <a href="#us">Mascotas</a>
+            <ul>
+              <li><a href="">Mestizos</a></li>
+              <li><a href="">Chihuahuas</a></li>
+            </ul>
+          </li>
+          <li><a href="#services">Donaciones</a></li>
+          <li><a href="#faqs">Foster to adopt</a></li>
+          <li>
+            <a href="#contact" class="button">Contacto</a>
+            <div class="links">
+              <a href="tel: 3314148548" target="_blank"
+                ><i class="fas fa-phone"></i> 3314148548</a
+              >
+              <a href="mailto: roman@huellitasdiferentes.com" target="_blank"
+                ><i class="fas fa-envelope"></i>roman@huellitasdiferentes.com</a
+              >
+            </div>
+          </li>
+        </ul>
+      </div>
+      <div class="middle">
+        <div class="socials">
+          <a href="" target="_blank"><i class="fab fa-facebook"></i></a>
+          <a href="" target="_blank"><i class="fab fa-instagram"></i></a>
+        </div>
+      </div>
+    </footer>
---
+++ elements/header.php
@@ -0,0 +1,22 @@
+<header>
+  <nav>
+    <a href="index.html">
+      <picture>
+        <source media="(max-width: 768px)" srcset="img/logo.jpg" />
+        <img src="img/logo.jpg" alt="" />
+      </picture>
+    </a>
+
+    <ul class="menu">
+      <li><a href="index.html">Historia</a></li>
+      <li><a href="perritos.html">Mascotas</a></li>
+      <li><a href="donaciones.html">Donaciones</a></li>
+      <li><a href="adopciones.html">Foster to adopt</a></li>
+      <li><a href="contacto.html" class="button">Contacto</a></li>
+      <li>
+        <a href="" target="_blank"><i class="far fa-bag-shopping"></i></a>
+      </li>
+    </ul>
+    <a><i class="fa-solid fa-bars menu__button"></i></a>
+  </nav>
+</header>
