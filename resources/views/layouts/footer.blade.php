928ad825821ce723eb0ca48223b21630b5d345de
Fernando <fernando93d@gmail.com>
2024-06-21 14:19:44 -0600

    Fix link

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -6,18 +6,18 @@
             </figure>
         </a>
         <ul>
-            <li><a href="index.php#hero">Historia</a></li>
+            <li><a href="/#hero">Historia</a></li>
             <li>
-                <a href="perritos.php">Mascotas</a>
+                <a href="/perritos">Mascotas</a>
                 <ul>
-                    <li><a href="perritos.php">Mestizos</a></li>
-                    <li><a href="perritos.php">Chihuahuas</a></li>
+                    <li><a href="/perritos">Mestizos</a></li>
+                    <li><a href="/perritos">Chihuahuas</a></li>
                 </ul>
             </li>
-            <li><a href="donaciones.php">Donaciones</a></li>
-            <li><a href="adopciones.php">Foster to adopt</a></li>
+            <li><a href="/donaciones">Donaciones</a></li>
+            <li><a href="/adopciones">Foster to adopt</a></li>
             <li>
-                <a href="contacto.php" class="button">Contacto</a>
+                <a href="/contacto" class="button">Contacto</a>
                 <div class="links">
                     <a href="tel: 3314148548" target="_blank"><i class="fas fa-phone"></i> 3314148548</a>
                     <a href="mailto: roman@huellitasdiferentes.com" target="_blank"><i class="fas fa-envelope"></i>roman@huellitasdiferentes.com</a>
