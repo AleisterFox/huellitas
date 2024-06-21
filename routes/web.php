cda69433b5937abe46f0b0b3e4b19553ad666786
Fernando <fernando93d@gmail.com>
2024-06-21 14:28:48 -0600

    Admin login

--- routes/web.php
+++ routes/web.php
@@ -4,15 +4,21 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\MainController;
 
-Route::get('/', [MainController::class, 'index']);
-Route::get('/perritos', [MainController::class, 'perritos']);
-Route::get('/donaciones', [MainController::class, 'donaciones']);
-Route::get('/adopciones', [MainController::class, 'adopciones']);
-Route::get('/contacto', [MainController::class, 'contacto']);
-Route::get('/producto', [MainController::class, 'producto']);
-Route::get('/adoptar', [MainController::class, 'adoptar']);
-Route::get('/carrito', [MainController::class, 'carrito']);
-Route::get('/informacion', [MainController::class, 'informacion']);
-Route::get('/confirmacion', [MainController::class, 'confirmacion']);
-Route::get('/envio', [MainController::class, 'envio']);
-Route::get('/pago', [MainController::class, 'pago']);
+Auth::routes(['register' => false]);
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::middleware([])->group(function() {
+    Route::get('/', [MainController::class, 'index']);
+    Route::get('/perritos', [MainController::class, 'perritos']);
+    Route::get('/donaciones', [MainController::class, 'donaciones']);
+    Route::get('/adopciones', [MainController::class, 'adopciones']);
+    Route::get('/contacto', [MainController::class, 'contacto']);
+    Route::get('/producto', [MainController::class, 'producto']);
+    Route::get('/adoptar', [MainController::class, 'adoptar']);
+    Route::get('/carrito', [MainController::class, 'carrito']);
+    Route::get('/informacion', [MainController::class, 'informacion']);
+    Route::get('/confirmacion', [MainController::class, 'confirmacion']);
+    Route::get('/envio', [MainController::class, 'envio']);
+    Route::get('/pago', [MainController::class, 'pago']);
+});
